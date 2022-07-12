0dd4064da696554ecf0f9e2b81c5131a2040572d
liuliang <2936454817@qq.com>
2022-07-12 13:19:44 +0800

    7.11

--- src/test/src/laserOdometry.cpp
+++ src/test/src/laserOdometry.cpp
@@ -1,19 +1,41 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
+#include "common.h"
+#include "tic_toc.h"
 #include <mutex>
 #include <queue>
 #include <nav_msgs/Odometry.h>
 #include <nav_msgs/Path.h>
+#include <pcl/point_cloud.h>
+#include <pcl_conversions/pcl_conversions.h>
 using namespace std;
 
 
 mutex mBuf;
 int skipFrameNUM = 5;
+bool systemInited = false;
+int cornerPointsSharpNum = 0; //角点个数
+int surfPointsFlatNum = 0;   //面点个数
 queue<sensor_msgs::PointCloud2ConstPtr> cornerSharpBuf;
 queue<sensor_msgs::PointCloud2ConstPtr> cornerLessSharpBuf;
 queue<sensor_msgs::PointCloud2ConstPtr> surfFlatBuf;
 queue<sensor_msgs::PointCloud2ConstPtr> surfLessFlatBuf;
 queue<sensor_msgs::PointCloud2ConstPtr> fullPointsBuf;
+
+double timeCornerPointsSharp = 0;
+double timeCornerPointsLessSharp = 0;
+double timeSurfPointsFlat = 0;
+double timeSurfPointsLessFlat = 0;
+double timeLaserCloudFullRes = 0;
+
+pcl::PointCloud<PointType>::Ptr cornerPointsSharp(new pcl::PointCloud<PointType>());
+pcl::PointCloud<PointType>::Ptr cornerPointsLessSharp(new pcl::PointCloud<PointType>());
+pcl::PointCloud<PointType>::Ptr surfPointsFlat(new pcl::PointCloud<PointType>());
+pcl::PointCloud<PointType>::Ptr surfPointsLessFlat(new pcl::PointCloud<PointType>());
+
+pcl::PointCloud<PointType>::Ptr laserCloudFullRes(new pcl::PointCloud<PointType>());
+pcl::PointCloud<PointType>::Ptr cornerPointsSharp(new pcl::PointCloud<PointType>());
+pcl::PointCloud<PointType>::Ptr cornerPointsSharp(new pcl::PointCloud<PointType>());
 
 
 
@@ -99,7 +121,58 @@ int main(int argc, char **argv)
            !surfFlatBuf.empty() && !surfLessFlatBuf.empty() &&
            !fullPointsBuf.empty())
         {
-            
+            // 分别求出队列的第一个时间
+            timeCornerPointsSharp = cornerSharpBuf.front()->header.stamp.toSec();
+            timeCornerPointsLessSharp = cornerLessSharpBuf.front()->header.stamp.toSec();
+            timeSurfPointsFlat = surfFlatBuf.front()->header.stamp.toSec();
+            timeSurfPointsLessFlat = surfLessFlatBuf.front()->header.stamp.toSec();
+            timeLaserCloudFullRes = fullPointsBuf.front()->header.stamp.toSec();
+            // 因为同一帧的时间戳都是相同的,因此这里比较是否是同一帧
+            // 一般不会出发这个事件,因为在发布的时候都是五个消息同步发送的,他们的header都赋的一样的
+            if(timeCornerPointsSharp != timeLaserCloudFullRes ||
+               timeCornerPointsLessSharp != timeLaserCloudFullRes ||
+               timeSurfPointsFlat != timeLaserCloudFullRes ||
+               timeSurfPointsLessFlat != timeLaserCloudFullRes)
+               {
+                    ROS_INFO("unsync messeage!");
+                    ROS_BREAK();
+               }
+            // 分别将五个点云消息取出来，同时转成pcl的点云格式
+            mBuf.lock();
+            cornerPointsSharp->clear();
+            pcl::fromROSMsg(*cornerSharpBuf.front(),*cornerPointsSharp);
+            mBuf.unlock();
+
+            cornerPointsLessSharp->clear();
+            pcl::fromROSMsg(*cornerLessSharpBuf.front(),*cornerPointsLessSharp);
+            mBuf.unlock();
+
+            surfPointsFlat->clear();
+            pcl::fromROSMsg(*surfFlatBuf.front(),*surfPointsFlat);
+            mBuf.unlock();
+
+            surfPointsLessFlat->clear();
+            pcl::fromROSMsg(*surfLessFlatBuf.front(),*surfPointsLessFlat);
+            mBuf.unlock();
+
+            laserCloudFullRes->clear();
+            pcl::fromROSMsg(*fullPointsBuf.front(),*laserCloudFullRes);
+            mBuf.unlock();
+
+            TicToc t_whole;
+            // 一个什么也不干的初始化
+            if(!systemInited)
+            {
+                systemInited = true;
+                ROS_INFO("Initialzation finished \n");
+            }
+            else
+            {
+                // 取出比较突出的特征
+                int cornerPointsSharpNum = cornerPointsSharp->points.size(); 
+            }
+
+
         }
     }
 }
